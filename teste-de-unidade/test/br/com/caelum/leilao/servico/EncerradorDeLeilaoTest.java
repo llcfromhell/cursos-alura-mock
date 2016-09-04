0e8a1b4f665ddc80cc7f0771236d8a30143022bb
Lourenço Lima da Cunha <llc.from.hell@gmail.com>
2016-09-04 13:45:57 -0300

    aula 03

--- teste-de-unidade/test/br/com/caelum/leilao/servico/EncerradorDeLeilaoTest.java
+++ teste-de-unidade/test/br/com/caelum/leilao/servico/EncerradorDeLeilaoTest.java
@@ -1,14 +1,7 @@
 package br.com.caelum.leilao.servico;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.inOrder;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -29,12 +22,15 @@ public class EncerradorDeLeilaoTest {
 	private LeilaoDao daoFalso;
 	private EnviadorDeEmail carteiroFalso;
 	private EncerradorDeLeilao encerrador;
+	private Calendar dataEncerrada;
 
 	@Before
 	public void before() {
 		daoFalso = mock(LeilaoDao.class);
 		carteiroFalso = mock(EnviadorDeEmail.class);
 		encerrador = new EncerradorDeLeilao(daoFalso, carteiroFalso);
+		dataEncerrada = Calendar.getInstance();
+		dataEncerrada.set(2016,01,01);
 	}
 	
 	@Test
@@ -121,5 +117,66 @@ public class EncerradorDeLeilaoTest {
 		
 	}
 	
+	@Test
+	public void testaNaoParaQuandoGeraErro() {
+		
+		Leilao leilao1 = new CriadorDeLeilao().para("blink 182 album").naData(dataEncerrada).constroi();
+		Leilao leilao2 = new CriadorDeLeilao().para("the offspring album").naData(dataEncerrada).constroi();
+		
+		List<Leilao> leiloesAntigos = Arrays.asList(leilao1, leilao2);
+		
+		when(daoFalso.correntes()).thenReturn(leiloesAntigos);
+		doThrow(new RuntimeException()).when(daoFalso).atualiza(leilao1);
+		
+        encerrador.encerra();
+        
+        assertEquals(1, encerrador.getTotalEncerrados());
+        
+        // tem fazer o processo pro leilao 2 mesmo que de erro no anterior
+        verify(daoFalso).atualiza(leilao2);
+        verify(carteiroFalso).envia(leilao2);
+		
+	}
+	
+	@Test
+	public void testaErroAoEnviarEmailNaoInterrompe() {
+		
+		Leilao leilao1 = new CriadorDeLeilao().para("guitarra fender").naData(dataEncerrada).constroi();
+		Leilao leilao2 = new CriadorDeLeilao().para("bateria mapex").naData(dataEncerrada).constroi();
+		
+		List<Leilao> leiloesAntigos = Arrays.asList(leilao1, leilao2);
+		
+		when(daoFalso.correntes()).thenReturn(leiloesAntigos);
+		doThrow(new RuntimeException()).when(carteiroFalso).envia(leilao1);
+		
+        encerrador.encerra();
+        
+        assertEquals(1, encerrador.getTotalEncerrados());
+        
+        // tem fazer o processo pro leilao 2 mesmo que de erro no anterior
+        verify(daoFalso).atualiza(leilao2);
+        verify(carteiroFalso).envia(leilao2);
+		
+	}
+	
+	@Test
+	public void testaErroEmTodosOsAcessoAoDadoNaoEnviaEmail() {
+		
+		List<Leilao> leiloesAntigos = Arrays.asList(
+				new CriadorDeLeilao().para("guitarra fender").naData(dataEncerrada).constroi(),
+				new CriadorDeLeilao().para("bateria mapex").naData(dataEncerrada).constroi(),
+				new CriadorDeLeilao().para("contrabaixo fender").naData(dataEncerrada).constroi());
+		
+		when(daoFalso.correntes()).thenReturn(leiloesAntigos);
+		doThrow(new RuntimeException()).when(daoFalso).atualiza(any(Leilao.class));
+		
+        encerrador.encerra();
+        
+        assertEquals(0, encerrador.getTotalEncerrados());
+
+        verify(carteiroFalso, never()).envia(any(Leilao.class));
+		
+	}
+
 
 }
